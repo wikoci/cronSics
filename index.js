dd8c04a9ff3a0dfa2153d8085c5935ea8412929e
wikoci <74468439+wikoci@users.noreply.github.com>
2022-04-06 04:36:57 +0200

    init project

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+global._ = require("lodash");
+global.chalk = require("chalk");
+global.log = require('consola')
+const express = require('express');
+const app = express();
+
+
+require("./src") // Entry App
+
+
+
+app.listen(process.env.PORT, () => {
+    log.success(chalk.green("\nCron Sics is running on port : ") + process.env.PORT)
+})
