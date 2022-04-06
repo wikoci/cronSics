31be5f7af89745aa3fea83d53983edad4598ab0a
wikoci <74468439+wikoci@users.noreply.github.com>
2022-04-06 13:05:53 +0200

    push

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+var cron = require("node-cron");
 const { allCommands } = require("./queries"); // List of commands
 const { connection, recentCrons, sequelize } = require("./db");
 
-
 (async() => {
-
-
     // await connection()
     allCommands(); // cmd for get all commands
+
+    var task = cron.schedule("*/2 * * * *", () => {
+        console.log("running a task [All commands /Order] every two minutes ");
+        allCommands(); // cmd for get all commands
+    }, {
+        scheduled: true
+    });
+
+    task.start();
 })();
